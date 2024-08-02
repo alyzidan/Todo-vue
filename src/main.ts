d040404da77f5d1a81fe85f2d2d47f49c1ade96c
AlyZidanDev <74658208+AlyZidanDev@users.noreply.github.com>
2024-08-02 16:15:12 +0300

    adding prime vue and configs

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  .mount("#app");
